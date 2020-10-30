3df0da6dd7c03de4c40f3e52c9725ebab74d15b8
Dain Brownlow <dain.brownlow@outlook.com>
2020-10-30 00:13:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
  x = 0
  new_numbers = []
  while x < numbers.length
-  new_numbers[x] = numbers[x] ** numbers[x]
+  new_numbers[x] = numbers[x] ** 2
   x += 1
  end
  return new_numbers
